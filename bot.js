5c12abb6c9687e9d0446bb811e1533ab69ab4966
sayyad41 <50000878+sayyad41@users.noreply.github.com>
2019-05-07 07:01:11 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on("message", message => {
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' ');
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-  m.send(`.{argresult}\n .{m}`);
+  m.send(`${argresult}\n ${m}`);
   })
   message.channel.send(`\`.{message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
   message.delete();
