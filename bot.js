6f9a7456147cb1d44d952ea1ab4a6d8de9bb2e4d
sayyad41 <50000878+sayyad41@users.noreply.github.com>
2019-05-07 07:05:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,18 +97,18 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
-      message.channel.send(`**✅   .{argresult}**`)
+      message.channel.send(`**✅   ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + '.setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   .{argresult}**`)
+      message.channel.send(`**✅   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + '.setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   .{argresult}**`)
+      message.channel.send(`**✅   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + '.sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
@@ -116,11 +116,11 @@ client.on('message', message => {
   }
   if (message.content.startsWith(adminprefix + '.setname')) {
   client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**.{argresult}** `)
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
 if (message.content.startsWith(adminprefix + 'setava')) {
   client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**.{argresult}** `);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
 
